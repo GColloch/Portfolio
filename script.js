2f9de626243d8a3f572ba111261bb3417ac7a3f0
Guillaume <guillaumecolloch56@gmail.com>
2022-11-06 21:24:19 +0100

    ajout des fichiers

---
+++ script.js
@@ -0,0 +1,26 @@
+// NAVBAR
+let lastScrollTop = 0;
+navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', function() {
+    const scrollTop = window.pageTOffset ||
+    this.document.documentElement.scrollTop;
+
+    if (scrollTop > lastScrollTop) {
+        navbar.style.top="-50px";
+    } else {
+        navbar.style.top="0";
+    }
+    lastScrollTop = scrollTop;
+})
+
+// TYPED
+var typed = new Typed('.typed', {
+    strings: ['Etudiant en 3em année d’un Bachelor Web et Multimédia à My Digital School Nantes, je suis passioné par le domaine du développement web. Je pratique également du montage vidéo et du bodyboard pour me détendre',],
+    typeSpeed: 25,
+    backSpeed: 0,
+    smartBackspace: true, // this is a default
+    loop: false
+  });
+// AOS
+AOS.init();
