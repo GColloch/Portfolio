3cae0d45d698357d79220cca220313e68c908127
Guillaume <guillaumecolloch56@gmail.com>
2023-01-17 11:06:02 +0100

    Modifications effectués

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TYPED
 var typed = new Typed('.typed', {
-    strings: ['Etudiant en 3em année d’un Bachelor Web et Multimédia à My Digital School Nantes, je suis passioné par le domaine du développement web. Je pratique également du montage vidéo et du bodyboard pour me détendre',],
+    strings: ['Etudiant en 3ème année d’un Bachelor Web et Multimédia à My Digital School Nantes, je suis passionné par le domaine du développement web. Je pratique également du montage vidéo et du bodyboard pour me détendre',],
     typeSpeed: 25,
     backSpeed: 0,
     smartBackspace: true, // this is a default
