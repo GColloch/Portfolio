55ab45baa9af7201a2284cc3ee4b1f71b32ee6ab
Guillaume <guillaumecolloch56@gmail.com>
2023-10-08 23:23:56 +0200

    ajout des diplomes et du texte

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TYPED
 var typed = new Typed('.typed', {
-    strings: ['Etudiant en 3ème année d’un Bachelor développement Web à My Digital School Nantes, je suis passionné par le domaine du développement web. Je pratique également du montage vidéo et du bodyboard pour me détendre',],
+    strings: ['Titulaire d\'une formation en développement web à My Digital School Nantes, j\'ai récemment décidé d\'opérer une réorientation vers un domaine qui m\'enthousiasme tout autant : l\'infrastructure réseau et la gestion des systèmes. C\'est pourquoi j\'ai saisi avec enthousiasme l\'opportunité d\'intégrer la formation du BTS SIO option SISR au sein de l\'IAA formation à Saint-Nazaire.',],
     typeSpeed: 25,
     backSpeed: 0,
     smartBackspace: true, // this is a default
