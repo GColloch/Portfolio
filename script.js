bb936f5fd42b6e0d0e986ae0306a482d80f70014
Guillaume <guillaumecolloch56@gmail.com>
2023-11-26 15:23:12 +0100

    CV a jour

--- script.js
+++ script.js
@@ -2,7 +2,7 @@
 
 // TYPED
 var typed = new Typed('.typed', {
-    strings: ['Titulaire d\'une formation en développement web à My Digital School Nantes, j\'ai récemment décidé d\'opérer une réorientation vers un domaine qui m\'enthousiasme tout autant : l\'infrastructure réseau et la gestion des systèmes. C\'est pourquoi j\'ai saisi avec enthousiasme l\'opportunité d\'intégrer la formation du BTS SIO option SISR au sein de l\'IAA formation à Saint-Nazaire.',],
+    strings: ['Titulaire d\'une formation en développement web à My Digital School Nantes, j\'ai récemment décidé d\'opérer une réorientation vers un domaine qui m\'enthousiasme tout autant : l\'infrastructure réseau et la gestion des systèmes. C\'est pourquoi j\'ai saisi avec enthousiasme l\'opportunité d\'intégrer la formation TSSR au sein de l\'ENI formation à Quimper.',],
     typeSpeed: 25,
     backSpeed: 0,
     smartBackspace: true, // this is a default
